825d037a9f35f4008d46c053544fc90d78720086
Gideon Wentink <gjwentink@gmail.com>
2014-11-01 18:14:06 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.h
+++ FlappyBird.spritebuilder/Source/GamePlayScene.h
@@ -13,7 +13,6 @@ typedef NS_ENUM(NSInteger, DrawingOrder) {
     DrawingOrderPipes,
     DrawingOrderGround,
     DrawingOrderHero
-    float timeSinceObstacle;
 };
 
 @interface GamePlayScene : CCNode <CCPhysicsCollisionDelegate>
@@ -22,6 +21,7 @@ typedef NS_ENUM(NSInteger, DrawingOrder) {
 
     Character*     character;
     CCPhysicsNode* physicsNode;
+    float timeSinceObstacle;
 
 }
 -(void) initialize;
